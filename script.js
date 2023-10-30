257e1c3b4b789b59bc7ff15723e4044a326b6a4a
Bulat <bulat0talipov@gmail.com>
2023-10-30 17:40:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+  modalLetaiCards();
+})
+
+
+function modalLetaiCards() {
+  const modal = document.querySelector('.modal-letai-cards')
+  const showModalBtn = document.querySelectorAll('.letai-cards__item-btn')
+  if (!modal && showModalBtn.length == 0) return
+
+  const modalBox = document.querySelector('.modal-letai-cards__box')
+  const closeModalBtn = document.querySelector('.modal-letai-cards__close-btn')
+
+
+  document.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.classList.contains("letai-cards__item-btn")) {
+      const modalBoxContent = document.querySelector('.modal-letai-cards__box-content');
+      modalBoxContent.innerHTML = target.closest('.letai-cards__item').innerHTML;
+      const modal = document.querySelector('.modal-letai-cards')
+      modal.querySelector('.letai-cards__item-btn').style.display = "none";
+
+      modal.classList.add('is-open');
+      modalBox.classList.add('modal-open');
+    }
+
+  })
+
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+    modalBox.classList.remove('modal-open');
+
+    const modalBoxContent = document.querySelector('.modal-letai-cards__box-content');
+    modalBoxContent.innerHTML = "";
+  })
+
+  document.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.classList.remove('is-open');
+      modalBox.classList.remove('modal-open');
+
+      const modalBoxContent = document.querySelector('.modal-letai-cards__box-content');
+      modalBoxContent.innerHTML = "";
+    }
+  })
+}
